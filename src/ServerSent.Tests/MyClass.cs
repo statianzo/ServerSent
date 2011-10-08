7965163f046d5ca1e9d81f19c59011d5794d7678
Jason Staten <jstaten07@gmail.com>
2011-10-08 13:52:54 -0600

    Initial Commit

---
+++ src/ServerSent.Tests/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ServerSent.Tests
+{
+  public class MyClass
+  {
+    public MyClass()
+    {
+    }
+  }
+}
+
---
+++ src/ServerSent/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ServerSent
+{
+  public class MyClass
+  {
+    public MyClass()
+    {
+    }
+  }
+}
+
