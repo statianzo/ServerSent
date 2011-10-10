96fa2d839c9670fa0205e1105c17f2b3cb080f48
Jason Staten <jstaten07@gmail.com>
2011-10-09 21:59:35 -0600

    Added event stream writer + tests

---
+++ src/ServerSent.Tests/EventStreamWriterTests.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+using System.IO;
+using System.Text;
+using Moq;
+
+namespace ServerSent.Tests
+{
+    public class EventStreamWriterTests
+    {
+        readonly EventStreamWriter _writer;
+        readonly MemoryStream _stream;
+        
+        public EventStreamWriterTests()
+        {
+            _stream = new MemoryStream();
+            _writer = new EventStreamWriter(_stream);
+        }
+    
+        [Fact]
+        public void ShouldWriteToStream()
+        {
+            const string input = "All of this should be there";
+            const string expected = "data: " + input + "\r\n\r\n";
+            var expectedBytes = Encoding.UTF8.GetBytes(expected);
+            
+            _writer.Write(input);
+            
+            var bytes = _stream.ToArray();
+            
+            
+            Assert.Equal(expectedBytes, bytes);
+        }
+        
+        [Fact]
+        public void ShouldWriteNamedEventToStream()
+        {
+            const string input = "Body of the event";
+            const string name = "dude";
+            const string expected = "event: " + name + "\r\n" +
+                                    "data: " + input + "\r\n\r\n";
+            var expectedBytes = Encoding.UTF8.GetBytes(expected);
+            
+            _writer.Write(input, name);
+            
+            var bytes = _stream.ToArray();
+            
+            
+            Assert.Equal(expectedBytes, bytes);
+        }
+        
+        [Fact]
+        public void ShouldWriteMultiLineEvent()
+        {
+            const string input = "Body\r\nof\r\nthe\r\nevent";
+            const string expected = "data: Body\r\n" +
+                                    "data: of\r\n" +
+                                    "data: the\r\n" +
+                                    "data: event\r\n\r\n";
+                                    
+            var expectedBytes = Encoding.UTF8.GetBytes(expected);
+            
+            _writer.Write(input);
+            
+            var bytes = _stream.ToArray();
+            
+            
+            Assert.Equal(expectedBytes, bytes);
+        }
+        
+        [Fact]
+        public void DisposeDisposesInnerStream()
+        {
+            _writer.Dispose();
+            Assert.False(_stream.CanWrite);
+        }
+        
+        [Fact]
+        public void ThrowsWhenUsedAfterDisposal()
+        {
+            _writer.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => _writer.Write(""));
+        }
+    }
+}
+
---
+++ src/ServerSent/EventStreamWriter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace ServerSent
+{
+    public class EventStreamWriter : IDisposable
+    {
+        StreamWriter _streamWriter;
+        bool _alreadyDisposed;
+        
+        public EventStreamWriter(Stream stream)
+        {
+            _streamWriter = new StreamWriter(stream);
+        }
+        
+        public void Write(string content, string eventName = null)
+        {
+            if (_alreadyDisposed)
+                throw new ObjectDisposedException("EventStreamWriter");
+                
+            var lines = content.Split(new []{'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            
+            if (eventName != null)
+                _streamWriter.Write("event: {0}\r\n", eventName);
+            
+            foreach (var line in lines)
+                _streamWriter.Write("data: {0}\r\n", line);
+            
+            _streamWriter.Write("\r\n");
+            _streamWriter.Flush();
+        }
+
+        public void Dispose()
+        {
+            if (_alreadyDisposed)
+                return;
+                
+            _alreadyDisposed = true;
+            _streamWriter.Dispose();
+            _streamWriter = null;
+        }
+    }
+}
+
